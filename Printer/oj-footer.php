024d3f4cd354608a5b1fe62a6286726a1de89555
DragonLee <840877228@qq.com>
2019-04-17 23:37:46 +0800

    Source Code

---
+++ Printer/oj-footer.php
@@ -0,0 +1,11 @@
+<?php
+      header("location:loginpage.php");
+      exit();
+?>
+
+<?php 
+require_once("include/db_info.inc.php");
+if(file_exists(dirname(__FILE__)."/template/".$OJ_TEMPLATE."/oj-footer.php"))
+  require_once(dirname(__FILE__)."/template/".$OJ_TEMPLATE."/oj-footer.php");
+
+?>
